3a0507baa04c85cd2e11ccea4d9464f5c21410d6
Ricardo McPherson <rmcpherson@fslads.fsl.org.jm>
2021-05-02 17:05:50 -0500

    Add buytokens functionality

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -4,8 +4,22 @@ import "./Token.sol";
 contract EthSwap{
 string public name  = "EthSwap Instant Exchange";
 Token public token;
+uint rate = 100;
 
-constructor() public{
-    
+event TokenPurchased(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+);
+constructor(Token _token) public{
+token = _token;
+}
+
+function buyTokens() public payable{
+    uint tokenAmount = msg.value * rate;
+    token.transfer(msg.sender, tokenAmount);
+
+    emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
 }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -9,11 +9,11 @@ function tokens(n){
     return web3.utils.toWei(n, 'ether')
 }
 
-contract('EthSwap', (accounts)=>{
+contract('EthSwap', ([deployer, investor])=>{
     let token, ethSwap
 before(async()=>{
     token = await Token.new()
-    ethSwap = await EthSwap.new()
+    ethSwap = await EthSwap.new(token.address)
     await token.transfer(ethSwap.address, tokens('1000000'))
 })
 describe('Token deployment', async()=>{
@@ -32,6 +32,25 @@ describe('EthSwap deployment', async()=>{
         let balance = await token.balanceOf(ethSwap.address)
         assert.equal(balance.toString(), tokens('1000000'))
         
+    })
+})
+
+
+describe('buyTokens', async()=>{
+    let result
+    before(async()=>{
+        await ethSwap.buyTokens({from : investor, value:tokens('1')});
+    })
+
+    it('Allows user to purchase tokens from ethSwap for a fixed price', async()=>{
+      let investorBalance =  await token.balanceOf(investor)
+      assert.equal(investorBalance.toString(), tokens('100'))
+
+      let ethSwapBalance
+      ethSwapBalance = await token.balanceOf(ethSwap.address)
+      assert.equal(ethSwapBalance.toString(), tokens('999900'))
+      ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+      assert.equal(ethSwapBalance.toString(), web3.utils.toWei('1','ether'))
     })
 })
 })
